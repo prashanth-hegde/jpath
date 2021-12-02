8ddac2b633a9055d6b38cd9364ffb3caa5029ee0
Prashanth.Hegde <Prashanth.Hegde@target.com>
2021-12-02 11:26:08 -0600

    parse input json with multi doc array in stdin - example using jpath as a pipe for kafka output

--- input/input.go
+++ input/input.go
@@ -3,6 +3,7 @@ package input
 import (
 	"bufio"
 	"bytes"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"io/ioutil"
@@ -10,20 +11,15 @@ import (
 	"os"
 )
 
+// ParseInputJson reads the input and makes a json out of it
 func ParseInputJson(json string) ([][]byte, error) {
 	var parsedb []byte
 	if json == "" {
-		log.Debugln("json input not provided, fetching from stdin")
-		reader := bufio.NewReader(os.Stdin)
-		var out []byte
-		for {
-			ru, _, err := reader.ReadRune()
-			if err != nil && err == io.EOF {
-				break
-			}
-			out = append(out, byte(ru))
+		p, e := readMultiDocumentArray()
+		if e != nil {
+			return nil, e
 		}
-		parsedb = out
+		parsedb = p
 	} else if json[0] == '{' || json[0] == '[' {
 		log.Debugln("valid json, parsing it")
 		parsedb = []byte(json)
@@ -37,4 +33,66 @@ func ParseInputJson(json string) ([][]byte, error) {
 	}
 
 	return common.Tokenize(parsedb)
+}
+
+// readMultiDocumentArray - get the json documents from stdin
+// One limitation with the underlying library is that it parses fully
+// formed json, and ignores the rest. For example, consider this input:
+//
+// {"name": "Abraham Lincoln": "quote": "Whatever you are, be a good one"}
+// {"name": "Oscar Wilde": "quote": "I can resist everything except temptation"}
+//
+// Notice the above input has a new line separator, and are two independent json objects
+// The jsonparser library only takes the first one if we provide this as an input
+// So we need to be a little proactive and wrap these objects into a json array
+// There are multiple approaches for doing this
+// 1. separate by new lines --> not a great assumption to make that input json will always be compressed
+// 2. parse the input manually, wrap the input objects into an array and tokenize them --> more code, but reliable
+func readMultiDocumentArray() ([]byte, error) {
+	reader := bufio.NewReader(os.Stdin) // stdin reader
+	documents := make([][]byte, 0)      // output documents
+	var parenthesis []bool              // parenthesis stack
+	var document []byte                 // current document
+
+	for {
+		// read each byte
+		input, err := reader.ReadByte()
+		if err != nil && err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, errors.Wrap(err, "error while reading stdin")
+		}
+
+		// trim whitespace if it's not part of the json
+		if len(parenthesis) == 0 && (input == ' ' || input == '\n' || input == '\t') {
+			continue
+		} else {
+			document = append(document, input)
+		}
+
+		// keep track of parenthesis
+		switch input {
+		case '[', '{':
+			parenthesis = append(parenthesis, true)
+		case ']', '}':
+			if len(parenthesis) == 0 {
+				return nil, common.InvalidJson.Error()
+			}
+			parenthesis = parenthesis[1:]
+		default:
+		}
+
+		// append finalized documents
+		if len(parenthesis) == 0 && len(document) > 0 {
+			// if the last parenthesis is closed, add this to the array and clear the document
+			documents = append(documents, document)
+			document = nil
+		}
+	}
+	// if parenthesis is not closed after reading the whole stdin, that's an error
+	if len(parenthesis) > 0 {
+		return nil, common.InvalidJson.Error()
+	}
+
+	return common.WrapIntoArray(documents), nil
 }
