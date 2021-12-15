cefba1ebff7d942b57ca5468fafe3fd804c8516a
Prashanth.Hegde <Prashanth.Hegde@target.com>
2021-12-15 07:50:11 -0600

    Resolve race condition in jpathAppRunner
    During unwrap output, there was this error that was occasionally coming up that mentioned the wait group was negative. Upon running some manual tests, it was found that the channel sometimes had empty lines in it. Not sure what causes those empty lines to be emitted to the channel, but ensuring we are decrementing the wait group only on non-empty channel inputs ensures that we are not running into negative wg counts

--- jpath.go
+++ jpath.go
@@ -34,15 +34,10 @@ func jpathAppRunner(parsedJson []byte) {
 func streamOutput(outChannel <-chan []byte) {
 	for {
 		doc := <-outChannel
-		//if len(doc) > 0 {
-		// fixme: I found some cases where byte array may need to be copied
-		// fixme: more testing needed
-		//tmp := make([]byte, len(doc))
-		//copy(tmp, doc)
-		//jpathAppRunner(tmp)
-		jpathAppRunner(doc)
-		common.Conf.Wg.Done()
-		//}
+		if len(doc) > 0 {
+			jpathAppRunner(doc)
+			common.Conf.Wg.Done()
+		}
 	}
 }
 
